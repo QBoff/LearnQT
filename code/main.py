14b8085fac2af2f25e39365c2c4767e3edd8115b
Dany <86149558+QBoff@users.noreply.github.com>
2022-09-21 17:09:52 +0300

    make some changes in main.py

--- code/main.py
+++ code/main.py
@@ -1,138 +1,112 @@
 import sys
+import traceback
 
+from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import *
+
+# Унаследуем наш класс от простейшего графического примитива QWidget
+
+
+def excepthook(exc_type, exc_value, exc_tb):
+    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
+    print("error catched!:")
+    print("error message:\n", tb)
+    QtWidgets.QApplication.quit()
+    # or QtWidgets.QApplication.exit(0)
+
+
+sys.excepthook = excepthook
 
 
 class Example(QWidget):
     def __init__(self):
+        # Надо не забыть вызвать инициализатор базового класса
         super().__init__()
-
-        # elements of out design
-        self.eq = QPushButton("=", self)
-        self.point = QPushButton(".", self)
-        self.CE = QPushButton("CE", self)
-        self.zero = QPushButton("0", self)
-        self.C = QPushButton("C", self)
-        self.plus = QPushButton("+", self)
-        self.minus = QPushButton("-", self)
-        self.umnosh = None
-        self.delim = QPushButton("/", self)
-        self.first_nums = QPlainTextEdit(self)
+        # В метод initUI() будем выносить всю настройку интерфейса,
+        # чтобы не перегружать инициализатор
         self.cheque = QPlainTextEdit(self)
-
-        self.string = ""
-        self.first_nums_string = ""
-        self.initUI()  # here will be all settings about our design
+        self.order = QPushButton("Заказать", self)
+        self.check_box1 = QCheckBox("Чизбургер", self)
+        self.line_edit1 = QLineEdit("0", self)
+        self.check_box2 = QCheckBox("Гамбургер", self)
+        self.line_edit2 = QLineEdit("0", self)
+        self.check_box3 = QCheckBox("Кока-кола", self)
+        self.line_edit3 = QLineEdit("0", self)
+        self.check_box4 = QCheckBox("Нагетсы", self)
+        self.line_edit4 = QLineEdit("0", self)
+        self.checkboxes = []
+        self.initUI()
 
     def initUI(self):
-        self.setGeometry(550, 200, 600, 500)
+        # Зададим размер и положение нашего виджета,
+        self.setGeometry(550, 200, 600, 300)
         self.setWindowTitle("Прятки для виджетов")
-        sp = [7, 8, 9, 4, 5, 6, 1, 2, 3]
-        self.string = ""
-        self.first_nums_string = ""
-        self.cheque.resize(600, 20)
-        self.cheque.move(0, 0)
 
-        self.first_nums.resize(600, 50)
-        self.first_nums.move(0, 20)
+        self.check_box1.move(20, 5)
 
-        x, y = 175, 70
+        self.line_edit1.resize(40, 20)
+        self.line_edit1.move(130, 5)
+        self.line_edit1.setEnabled(False)
+        self.check_box1.stateChanged.connect(
+            lambda: self.isCh([self.check_box1, self.line_edit1, 10]))
+        self.checkboxes.append([self.check_box1, self.line_edit1, 10])
 
-        for i in range(1, 10):
-            btn = QPushButton(str(sp[i - 1]), self)
-            btn.resize(70, 70)
-            btn.move(x, y)
-            btn.clicked.connect(self.add_all)
-            btn.clicked.connect(self.fr_num)
-            if i % 3 == 0:
-                x = 175
-                y += 70
-            else:
-                x += 70
+        self.check_box2.move(20, 35)
+        # self.checkboxes.append(self.check_box2)
 
-        self.delim.resize(70, 70)
-        self.delim.move(385, 70)
-        self.delim.clicked.connect(self.add_all)
-        self.delim.clicked.connect(self.fr_num)
+        self.line_edit2.resize(40, 20)
+        self.line_edit2.move(130, 35)
+        self.line_edit2.setEnabled(False)
+        self.check_box2.stateChanged.connect(
+            lambda: self.isCh([self.check_box2, self.line_edit2, 10]))
+        self.checkboxes.append([self.check_box2, self.line_edit2, 20])
 
-        self.umnosh = QPushButton("*", self)
-        self.umnosh.resize(70, 70)
-        self.umnosh.move(385, 140)
-        self.umnosh.clicked.connect(self.add_all)
-        self.umnosh.clicked.connect(self.fr_num)
+        self.check_box3.move(20, 65)
+        # self.checkboxes.append(self.check_box3)
 
-        self.minus.resize(70, 70)
-        self.minus.move(385, 210)
-        self.minus.clicked.connect(self.add_all)
-        self.minus.clicked.connect(self.fr_num)
+        self.line_edit3.resize(40, 20)
+        self.line_edit3.move(130, 65)
+        self.line_edit3.setEnabled(False)
+        self.check_box3.stateChanged.connect(
+            lambda: self.isCh([self.check_box3, self.line_edit3, 10]))
+        self.checkboxes.append([self.check_box3, self.line_edit3, 25])
 
-        self.plus.resize(70, 70)
-        self.plus.move(385, 280)
-        self.plus.clicked.connect(self.add_all)
-        self.plus.clicked.connect(self.fr_num)
+        self.check_box4.move(20, 95)
+        # self.checkboxes.append(self.check_box4)
 
-        self.C.resize(70, 70)
-        self.C.move(175, 280)
-        self.C.clicked.connect(self.c)
+        self.line_edit4.resize(40, 20)
+        self.line_edit4.move(130, 95)
+        self.line_edit4.setEnabled(False)
+        self.check_box4.stateChanged.connect(
+            lambda: self.isCh([self.check_box4, self.line_edit4, 10]))
+        self.checkboxes.append([self.check_box4, self.line_edit4, 30])
 
-        self.zero.resize(70, 70)
-        self.zero.move(245, 280)
-        self.zero.clicked.connect(self.add_all)
-        self.zero.clicked.connect(self.fr_num)
+        self.order.resize(150, 50)
+        self.order.move(20, 135)
+        self.order.clicked.connect(self.tocheque)
 
-        self.CE.resize(70, 70)
-        self.CE.move(315, 280)
-        self.CE.clicked.connect(self.ce)
+        self.cheque.resize(320, 200)
+        self.cheque.move(230, 10)
+        self.cheque.setEnabled(False)
 
-        self.point.resize(70, 70)
-        self.point.move(175, 350)
-        self.point.clicked.connect(self.add_all)
-        self.point.clicked.connect(self.fr_num)
+    def tocheque(self):
+        orders = "Ваш заказ: \n\n"
+        total = 0
+        for item in self.checkboxes:
+            if item[0].isChecked():
+                orders += f"{item[0].text()}----{item[1].text()}---{int(item[2]) * int(item[1].text())}\n"
+                total += int(item[2]) * int(item[1].text())
 
-        self.eq.resize(140, 70)
-        self.eq.move(315, 350)
-        self.eq.clicked.connect(self.result)
+        orders += f"\nИтого: {total}!!!\nНе ешьте много!"
+        self.cheque.setPlainText(
+            orders
+        )
 
-    def add_all(self):
-        self.string += self.sender().text()
-        self.cheque.setPlainText(self.string)
-        print(self.string)
-
-    def fr_num(self):
-
-        if self.sender().text() in "+-*/":
-            self.first_nums_string = ""
-        else:
-            self.first_nums_string += self.sender().text()
-
-        self.first_nums.setPlainText(self.first_nums_string)
-
-    def result(self):
-        try:
-            res = eval(self.string)
-            if '.' in self.string:
-                self.string = str(float(res))
-                self.cheque.setPlainText(self.string)
-                self.first_nums_string = str(float(res))
-                self.first_nums.setPlainText(self.first_nums_string)
-            else:
-                self.string = str(int(res))
-                self.cheque.setPlainText(self.string)
-                self.first_nums_string = str(int(res))
-                self.first_nums.setPlainText(self.first_nums_string)
-
-        except Exception:
-            self.first_nums.setPlainText("Error")
-
-    def ce(self):
-        self.cheque.setPlainText("")
-        self.first_nums.setPlainText("")
-        self.string = ""
-        self.first_nums_string = ""
-
-    def c(self):
-        self.first_nums.setPlainText("")
-        self.first_nums_string = ""
+    @staticmethod
+    def isCh(ed):
+        if ed[0].isChecked():
+            ed[1].setText("1")
+            ed[1].setEnabled(True)
 
 
 if __name__ == '__main__':
@@ -140,6 +114,17 @@ if __name__ == '__main__':
     app = QApplication(sys.argv)
     # А теперь создадим и покажем пользователю экземпляр
     # нашего виджета класса Example
+    app.setStyle("fusion")
+    app.setStyleSheet("""
+                      QCheckBox {
+                        spacing: 7px;
+                        font-size: 15px;     
+                    }
+                     QCheckBox::indicator {
+                        width: 40px
+                        height: 40px
+                    }
+                      """)
     ex = Example()
     ex.show()
     # Будем ждать, пока пользователь не завершил исполнение QApplication,
