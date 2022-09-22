12cb902393825edb61a664e2acbfa01f6f5c038a
Dany <86149558+QBoff@users.noreply.github.com>
2022-09-22 16:52:37 +0300

    I have done 3 task from 3 block

--- sirius_tasks/task3_block3.py
+++ sirius_tasks/task3_block3.py
@@ -7,13 +7,15 @@ t = 2
 if n == 1:
     print(1, 1)
 else:
-    divisors = [n, 1]
-
-    while t < ceil(sqrt(n)):
-        if n % t == 0:
-            divisors.append(t)
-            divisors.append(n // t)
-
-        t += 1
-
-    print(len(divisors), sum(divisors))
+    count, summ = 0, 0
+    
+    for i in range(1, int(sqrt(n)) + 1):
+        if n % i == 0 and i != sqrt(n):
+            count += 2
+            summ += (i + n // i)
+        elif n % i == 0 and i == sqrt(n):
+            count += 1
+            summ += i
+    print(int(count), int(summ))
+    print(type(ceil(sqrt(n))))
+    print(ceil(sqrt(n)), int(sqrt(n)) + 1)
