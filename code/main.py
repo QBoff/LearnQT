39ad3f4f94fe2ad78f2ac0d57d91814a7cf0580a
Dany <86149558+QBoff@users.noreply.github.com>
2022-09-28 21:21:09 +0300

    add some changes

--- code/main.py
+++ code/main.py
@@ -1,112 +1,93 @@
-import sys
-import traceback
+import re
+import string
 
-from PyQt5 import QtWidgets
-from PyQt5.QtWidgets import *
-
-# Унаследуем наш класс от простейшего графического примитива QWidget
+number = input()
 
 
-def excepthook(exc_type, exc_value, exc_tb):
-    tb = "".join(traceback.format_exception(exc_type, exc_value, exc_tb))
-    print("error catched!:")
-    print("error message:\n", tb)
-    QtWidgets.QApplication.quit()
-    # or QtWidgets.QApplication.exit(0)
+class formatException(Exception):
+    pass
 
 
-sys.excepthook = excepthook
+class wrongCountryCode(Exception):
+    pass
 
 
-class Example(QWidget):
-    def __init__(self):
-        # Надо не забыть вызвать инициализатор базового класса
-        super().__init__()
-        # В метод initUI() будем выносить всю настройку интерфейса,
-        # чтобы не перегружать инициализатор
-        self.list_names = ["Чизбургер", "Гамбургер", "Кока-кола", "Нагетсы"]
-        self.cheque = QPlainTextEdit(self)
-        self.order = QPushButton("Заказать", self)
-        # self.check_box1 = QCheckBox("Чизбургер", self)
-        # self.line_edit1 = QLineEdit("0", self)
-        # self.check_box2 = QCheckBox("Гамбургер", self)
-        # self.line_edit2 = QLineEdit("0", self)
-        # self.check_box3 = QCheckBox("Кока-кола", self)
-        # self.line_edit3 = QLineEdit("0", self)
-        # self.check_box4 = QCheckBox("Нагетсы", self)
-        # self.line_edit4 = QLineEdit("0", self)
-        self.checkboxes = []
-        self.initUI()
-
-    def initUI(self):
-        # Зададим размер и положение нашего виджета,
-        self.setGeometry(550, 200, 600, 300)
-        self.setWindowTitle("Прятки для виджетов")
-
-        self.move_checkbox()
-
-        self.order.resize(150, 50)
-        self.order.move(20, 135)
-        self.order.clicked.connect(self.tocheque)
-
-        self.cheque.resize(320, 200)
-        self.cheque.move(230, 10)
-        self.cheque.setEnabled(False)
-
-    def tocheque(self):
-        orders = "Ваш заказ: \n\n"
-        total = 0
-        for item in self.checkboxes:
-            if item[0].isChecked():
-                orders += f"{item[0].text()}----{item[1].text()}---{int(item[2]) * int(item[1].text())}\n"
-                total += int(item[2]) * int(item[1].text())
-
-        orders += f"\nИтого: {total}!!!\nНе ешьте много!"
-        self.cheque.setPlainText(
-            orders
-        )
+class longNumberException(Exception):
+    pass
 
 
-    def isCh(self):
-        if self.sender().isChecked():
-            for e in self.checkboxes:
-                if self.sender() == e[0]:
-                    e[1].setText("1")
-                    e[1].setEnabled(True)
-    
-    def move_checkbox(self) -> None:
-        x, x2, y = 20, 130, 5
-        for i in range(4):
-            self.check_box = QCheckBox(self.list_names[i], self)
-            self.check_box.move(x, y)
-            
-            self.line_edit1 = QLineEdit("0", self)
-            self.line_edit1.resize(40, 20)
-            self.line_edit1.move(x2, y)
-            self.line_edit1.setEnabled(False)
-            self.check_box.stateChanged.connect(self.isCh)
-            self.checkboxes.append([self.check_box, self.line_edit1, 10])
-            
-            y += 20
+class wrongOperatorCode(Exception):
+    pass
 
-if __name__ == '__main__':
-    # Создадим класс приложения PyQT
-    app = QApplication(sys.argv)
-    # А теперь создадим и покажем пользователю экземпляр
-    # нашего виджета класса Example
-    app.setStyle("fusion")
-    app.setStyleSheet("""
-                      QCheckBox {
-                        spacing: 7px;
-                        font-size: 15px;     
-                    }
-                     QCheckBox::indicator {
-                        width: 40px
-                        height: 40px
-                    }
-                      """)
-    ex = Example()
-    ex.show()
-    # Будем ждать, пока пользователь не завершил исполнение QApplication,
-    # а потом завершим и нашу программу
-    sys.exit(app.exec())
+
+try:
+    def check_country_code(number: str):
+        res = re.findall(r'^(\+7|8|\+359|\+55|\+1)', number)
+        if not res:
+            raise wrongCountryCode
+        return res
+
+    def checkStart(number: str):
+        res = re.findall(
+            r'^(\+7|8|\+359|\+55|\+1)\(?(\d{3})\)?\d{3}-?\d{4}$', number)
+        if not res:
+            raise formatException
+        return res
+
+    def bracketCheck(number: str):
+        if number.count('(') == number.count(')') == 1 and number.find('(') < number.find(')')\
+                or number.count('(') == number.count(')') == 0:
+            return True
+        # print("1213456ui")
+        raise formatException
+
+    def minusCheck(number: str):
+        if '-' in number and "--" not in number and number[-1] != '-' or '-' not in number:
+            return True
+        raise formatException
+
+    def checkWord(number: str):
+        for w in string.ascii_letters:
+            if w in number:
+                raise formatException
+
+        return True
+
+    def main():
+        global number
+        number = number.replace(' ', '').replace("\t", "").replace("\n", "")
+        # number = number.replace('(', '').replace(')', '').replace('-', '')
+        # print(number)
+        if all([checkStart(number), check_country_code(number), bracketCheck(number),
+                minusCheck(number), checkWord(number)]):
+            number = number.replace('(', '').replace(')', '').replace('-', '')
+            number = number.replace('+', '')
+            if len(number) != 11:
+                raise longNumberException
+                # quit()
+            if "7" == number[0]:
+                number = '+' + number
+            elif "8" == number[0]:
+                number = '+7' + number[1::]
+            # print(number[2:5])
+
+            if int(number[2:5]) not in range(910, 919 + 1) and int(number[2:5]) not in range(980, 989 + 1) \
+                    and int(number[2:5]) not in range(920, 939 + 1) and int(number[2:5]) not in range(902, 906 + 1) \
+                    and int(number[2:5]) not in range(960, 969 + 1):
+                raise wrongOperatorCode
+
+            print(number)
+        else:
+            raise formatException
+
+    if __name__ == '__main__':
+        main()
+
+except formatException:
+    print("неверный формат")
+except longNumberException:
+    print("неверное количество цифр")
+except wrongOperatorCode:
+    print("не определяется оператор сотовой связи")
+except wrongCountryCode:
+    print("не определяется код страны")
