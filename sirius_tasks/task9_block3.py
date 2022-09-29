952e05cbd5eababb152c1ee48c5a109397f6b55f
Dany <86149558+QBoff@users.noreply.github.com>
2022-09-29 19:34:50 +0300

    i've done 10 task

--- sirius_tasks/task9_block3.py
+++ sirius_tasks/task9_block3.py
@@ -1,15 +1,38 @@
-def gcdExtended(a, b):
-    if a == 0:
-        return b, 0, 1
-    gcd, x1, y1 = gcdExtended(b % a, a)
-    x = y1 - (b//a) * x1
-    y = x1
-    return gcd, x, y
+import math
 
 
-m, a = map(int, input().split())
-gcd, x, y = gcdExtended(a, m)
-if gcd == 1:
-    print((x % m + m) % m)
-else:
-    print(-1)
+def mutually_prime_numbers(a, b) -> bool:
+    while b > 0:
+        a, b = b, a % b
+
+    return a
+
+
+def factorization(n):
+    p = []
+    d = 1
+    while d * d <= n:
+
+        if n % d == 0:
+            p.append(d)
+            if n // d != d:
+                p.append(n // d)
+        d += 1
+
+    return p
+
+
+n = int(input())
+li = factorization(n)
+size = len(li)
+count = 0
+
+for i in range(size):
+    for j in range(i + 1, size):
+        a = li[i]
+        b = li[j]
+
+        if mutually_prime_numbers(a, b) == 1 and a * b <= n:
+            count += 1
+
+print(count)
