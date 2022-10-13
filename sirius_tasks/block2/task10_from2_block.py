b2bd4a55751f50bfbc55b40fb0b34c96e9bfb457
Dany <86149558+QBoff@users.noreply.github.com>
2022-10-13 21:40:39 +0300

    com some changes

--- sirius_tasks/block2/task10_from2_block.py
+++ sirius_tasks/block2/task10_from2_block.py
@@ -1,9 +1,9 @@
 from sys import setrecursionlimit
-setrecursionlimit(10**9)  # увеличение максимальной глубины рекурсии
+setrecursionlimit(10**9)
 
 
 def decomp(number):
-    n = number  # функция разложения числа на простые множители
+    n = number
     ans = []
     d = 2
     while d * d <= n:
@@ -18,19 +18,19 @@ def decomp(number):
 
 
 x = int(input())
-a = list(set(decomp(x)))  # разложение числа x на простые множители в единственном экземпляре
-b = decomp(x)  # разложение числа x на простые множители
+a = list(set(decomp(x)))
+b = decomp(x)
 
 y = 1
-for i in range(len(a)):  # перемножение простых множителей
+for i in range(len(a)):
     y *= a[i]
 k = 1
 n = k*y
 
-if x == 1:  # если x = 1, то и n = 1
+if x == 1:
     print(1)
-elif len(b) >= 30:  # 29 - потому что хотя бы 2**30 уже будет более 10**9, что противоречит условию задачи
-    print(y)  # тогда y**y уже будет делится на n
+elif len(b) >= 30:
+    print(y) 
 else:
     while pow(n, n, x) != 0:
         n = k*y
